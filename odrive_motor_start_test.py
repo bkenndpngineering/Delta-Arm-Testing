e3a2b7e78a83b39924d9899617ca01862fde6992
NoneYaBusiness <none@ya.business>
2019-10-04 08:25:11 -0700

    added some notes about odrive

--- odrive_motor_start_test.py
+++ odrive_motor_start_test.py
@@ -9,6 +9,10 @@ if not od:
     print("Did not find an ODrive")
     exit()
 
+# Find way to separate two ODrives by ID
+
+# First ODrive controller
+'''
 # od.axis0 --> motor 1
 ax0 = ODrive_Ease_Lib.ODrive_Axis(od.axis0)
 ax0.index_and_hold(-1, 1)    # works!!! up until this point
@@ -16,3 +20,10 @@ ax0.index_and_hold(-1, 1)    # works!!! up until this point
 # od.axis1 --> motor 2
 ax1 = ODrive_Ease_Lib.ODrive_Axis(od.axis1)
 ax1.index_and_hold(-1, 1)   # works!!! up until this point
+'''
+
+# Second ODrive controller
+
+# od.axis0 --> motor 3
+ax2 = ODrive_Ease_Lib.ODrive_Axis(od.axis0)
+ax2.index_and_hold(-1, 1)
